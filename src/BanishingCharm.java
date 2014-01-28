43196fd1ed7642c875c4e5d5b8a4e99b099a32a1
test <test@test.com>
2014-01-28 15:08:19 -0800

    Changed BanishingCharm

--- src/BanishingCharm.java
+++ src/BanishingCharm.java
@@ -3,6 +3,10 @@ package edu.hogwarts.SpellSim.Spells;
 class BanishingCharm implements Spell {
     public String name = "BanishingCharm";
 
+    public String getName () {
+		return name;
+	}
+
     public void invokeOn(Subject subject) {
         throw new NotImplementedError("Not yet implemented.");
     }
